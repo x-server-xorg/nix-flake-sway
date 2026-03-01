842c2715e4209b31e45062820a2a1570d308f940
x-server-xorg <129722080+x-server-xorg@users.noreply.github.com>
2026-03-01 14:09:50 +0300

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  time.timeZone = "Europe/Moscow";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  networking.firewall.enable = false;
+
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+
+
+  nix.gc.automatic = true;
+  nix.gc.dates = "daily"; 
+  nix.gc.options = "--delete-older-than 3d";
+
+  nix.optimise.automatic = true;
+  nix.optimise.dates = [ "dayly" ];
+
+}
