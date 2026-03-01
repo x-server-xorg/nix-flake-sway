588f4bd015d86d144353b188f9456c3a0084f235
x-server-xorg <129722080+x-server-xorg@users.noreply.github.com>
2026-03-01 14:30:49 +0300

    Update packages.nix

--- packages.nix
+++ packages.nix
@@ -1,7 +1,7 @@
 # packages.nix
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    sway waybar rofi-wayland alacritty firefox nemo gthumb git fastfetch
+    sway waybar rofi alacritty firefox nemo gthumb git fastfetch
   ];
   programs.sway.enable = true;
 }
